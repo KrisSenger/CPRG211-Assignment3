909e0edc2514d4c839dc1f3f395555e2a74161b4
KSsait <kristopher.senger@edu.sait.ca>
2024-03-28 19:48:59 -0600

    Fix assert statement in TestDeSerialization

--- Test_Assignment_3_1/SerializationTests.cs
+++ Test_Assignment_3_1/SerializationTests.cs
@@ -9,12 +9,13 @@ namespace Test_Assignment_3
 {
     public class SerializationTests
     {
-        private List<User> users;
+        private SingleLinkedList users;
         private readonly string testFileName = @"..\..\test_users.bin";
 
         [SetUp]
         public void Setup()
         {
+            users = new SingleLinkedList();
             users.Append(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
             users.Append(new User(2, "Joe Schmoe", "joe.schmoe@outlook.com", "abcdef"));
             users.Append(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
@@ -39,14 +40,19 @@ namespace Test_Assignment_3
         public void TestDeSerialization()
         {
             SerializationHelper.SerializeUsers(users, testFileName);
-            List<User> deserializedUsers = (List<User>)SerializationHelper.DeserializeUsers(testFileName);
-            Assert.AreEqual(users.Count, deserializedUsers.Count);
-            for (int i = 0; i < users.Count; i++)
+            SingleLinkedList deserializedUsers = (SingleLinkedList)SerializationHelper.DeserializeUsers(testFileName);
+
+            int userCount = users.Size();
+            int deserializedUserCount = deserializedUsers.Size();
+            Assert.That(userCount, Is.EqualTo(deserializedUserCount));
+
+            for (int i = 0; i < userCount; i++)
             {
-                Assert.AreEqual(users[i].Id, deserializedUsers[i].Id);
-                Assert.AreEqual(users[i].Name, deserializedUsers[i].Name);
-                Assert.AreEqual(users[i].Email, deserializedUsers[i].Email);
-                Assert.AreEqual(users[i].Password, deserializedUsers[i].Password);
+                Assert.That(users.Retrieve(i).ToString(), Is.EqualTo(deserializedUsers.Retrieve(i).ToString()));
+                // Assert.AreEqual(users[i].Id, deserializedUsers[i].Id);
+                // Assert.AreEqual(users[i].Name, deserializedUsers[i].Name);
+                // Assert.AreEqual(users[i].Email, deserializedUsers[i].Email);
+                // Assert.AreEqual(users[i].Password, deserializedUsers[i].Password);
             }
         }
 
